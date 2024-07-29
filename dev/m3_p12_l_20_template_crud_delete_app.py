26ddaed2d80310c64ec26b8c2c36dbe5383f6c77
akasmall <aka_sm@ro.ru>
2024-07-29 23:31:16 +0300

    сделал m3_p12_l_20_template_crud_delete_app

---
+++ dev/m3_p12_l_20_template_crud_delete_app.py
@@ -0,0 +1,113 @@
+import os
+from flask import (
+    Flask,
+    flash,
+    get_flashed_messages,
+    make_response,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
+
+from dev.m3_p12_l_20_template_crud_delete_repository import PostsRepository
+from dev.m3_p12_l_20_template_crud_delete_validator import validate
+# from flask import session
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+# app.config['SECRET_KEY'] = "SuperSecretKey"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.get('/posts')
+def posts_get():
+    repo = PostsRepository()
+    messages = get_flashed_messages(with_categories=True)
+    posts = repo.content()
+    return render_template(
+        'posts/index.html',
+        posts=posts,
+        messages=messages,
+    )
+
+
+@app.route('/posts/new')
+def new_post():
+    post = {}
+    errors = {}
+    return render_template(
+        'posts/new.html',
+        post=post,
+        errors=errors,
+    )
+
+
+@app.post('/posts')
+def posts_post():
+    repo = PostsRepository()
+    data = request.form.to_dict()
+    errors = validate(data)
+    if errors:
+        return render_template(
+            'posts/new.html',
+            post=data,
+            errors=errors,
+        ), 422
+    id_ = repo.save(data)
+    flash('Post has been created', 'success')
+    resp = make_response(redirect(url_for('posts_get')))
+    resp.headers['X-ID'] = id_
+    return resp
+
+
+@app.route('/posts/<id_>/update', methods=['GET', 'POST'])
+def post_update(id_):
+    repo = PostsRepository()
+    post = repo.find(id_)
+    errors = []
+
+    if request.method == 'GET':
+        return render_template(
+            'posts/edit.html',
+            post=post,
+            errors=errors,
+            data=post,
+        )
+
+    if request.method == 'POST':
+        data = request.form.to_dict()
+
+        errors = validate(data)
+        if errors:
+            return render_template(
+                'posts/edit.html',
+                post=post,
+                data=data,
+                errors=errors,
+            ), 422
+
+        post['title'] = data['title']
+        post['body'] = data['body']
+        repo.save(post)
+        flash('Post has been updated', 'success')
+        return redirect(url_for('posts_get'))
+
+
+# BEGIN (write your solution here)
+@app.route('/posts/<id_>/delete', methods=['GET', 'POST'])
+def delete_post(id_):
+    if request.method == 'POST':
+        repo = PostsRepository()
+        repo.destroy(id_)
+        flash('Post has been removed', 'success')
+
+        return redirect(url_for('posts_get'))
+    else:
+        return '', 405
+
+# END
---
+++ dev/m3_p12_l_20_template_crud_delete_repository.py
@@ -0,0 +1,32 @@
+import json
+import sys
+import uuid
+from flask import session
+
+
+class PostsRepository():
+    def __init__(self):
+        if 'posts' not in session:
+            session['posts'] = {}
+
+    def content(self):
+        return session['posts'].values()
+
+    def find(self, id_):
+        try:
+            return session['posts'][id_]
+        except KeyError:
+            sys.stderr.write(f'Wrong post id: {id_}')
+            raise
+
+    def destroy(self, id_):
+        del session['posts'][id_]
+
+    def save(self, post):
+        if not (post.get('title') and post.get('body')):
+            raise Exception(f'Wrong data: {json.loads(post)}')
+        if not post.get('id'):
+            post['id'] = str(uuid.uuid4())
+        session['posts'][post['id']] = post
+        session['posts'] = session['posts']
+        return post['id']
